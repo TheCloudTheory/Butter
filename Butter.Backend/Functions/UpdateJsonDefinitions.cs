78c8fda4fa4e84196e25349936a1df1868ca6748
Kamil Mrzygłód <kamil@thecloudtheory.com>
2019-10-02 22:11:24 +0200

    Now HTTP helper is no longer static and accepts logger

--- Butter.Backend/Functions/UpdateJsonDefinitions.cs
+++ Butter.Backend/Functions/UpdateJsonDefinitions.cs
@@ -15,19 +15,22 @@ namespace Butter.Backend
     {
         [FunctionName("UpdateJsonDefinitions")]
         public static async Task Run(
-            [TimerTrigger("0 */60 * * * *")] TimerInfo myTimer, 
+            [TimerTrigger("0 */60 * * * *")] TimerInfo myTimer,
             [Blob("schemas")] CloudBlobContainer container,
             ILogger log)
         {
             await container.CreateIfNotExistsAsync();
 
+            var http = new Http(log);
             var map = new Dictionary<string, List<string>>();
-            var schemas = await Http.GetSchemas();
+            var schemas = await http.GetSchemas();
 
-            foreach(var schema in schemas) {
-                var contents = await Http.GetContent(schema.Path);
-                foreach(var content in contents) {
-                    var file = await Http.GetFileContent($"{schema.Path}/{content.Name}");
+            foreach (var schema in schemas)
+            {
+                var contents = await http.GetContent(schema.Path);
+                foreach (var content in contents)
+                {
+                    var file = await http.GetFileContent($"{schema.Path}/{content.Name}");
                     var decodedFile = Encoding.UTF8.GetString(Convert.FromBase64String(file.Content));
 
                     var blob = container.GetBlockBlobReference($"{schema.Path}_{content.Name}");
@@ -35,10 +38,13 @@ namespace Butter.Backend
 
                     var mapKey = content.Name.Replace(".json", "");
                     var mapValue = schema.Path.Split('/')[1];
-                    if(map.ContainsKey(mapKey)) {
+                    if (map.ContainsKey(mapKey))
+                    {
                         map[mapKey].Add(mapValue);
-                    } else {
-                         map[mapKey] = new List<string> {
+                    }
+                    else
+                    {
+                        map[mapKey] = new List<string> {
                              mapValue
                          };
                     }
--- Butter.Backend/Http.cs
+++ Butter.Backend/Http.cs
@@ -2,40 +2,57 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace Butter.Backend
 {
-    public class Http {
+    public class Http
+    {
         private const string BaseGitHubUrl = "https://api.github.com/";
 
-        private static readonly Lazy<HttpClient> Client = new Lazy<HttpClient>(() => new HttpClient() {
+        private readonly ILogger _logger;
+
+        public Http(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        private static readonly Lazy<HttpClient> Client = new Lazy<HttpClient>(() => new HttpClient()
+        {
             DefaultRequestHeaders = {
                 {"User-Agent", "TheCloudTheory/Butter"}
             }
         });
 
-        public static async Task<IEnumerable<SchemaMetadata>> GetSchemas() {
+        public async Task<IEnumerable<SchemaMetadata>> GetSchemas()
+        {
             var schemas = await Get<IEnumerable<SchemaMetadata>>($"{BaseGitHubUrl}repos/Azure/azure-resource-manager-schemas/contents/schemas");
             return schemas;
         }
 
-        public static async Task<IEnumerable<SchemaMetadata>> GetContent(string path) {
+        public async Task<IEnumerable<SchemaMetadata>> GetContent(string path)
+        {
             var content = await Get<IEnumerable<SchemaMetadata>>($"{BaseGitHubUrl}repos/Azure/azure-resource-manager-schemas/contents/{path}");
             return content;
         }
 
-        public static async Task<File> GetFileContent(string path) {
+        public async Task<File> GetFileContent(string path)
+        {
             var file = await Get<File>($"{BaseGitHubUrl}repos/Azure/azure-resource-manager-schemas/contents/{path}");
             return file;
         }
 
-        private static async Task<T> Get<T>(string url) {
+        private async Task<T> Get<T>(string url)
+        {
             var clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
             var clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
 
             var response = await Client.Value.GetAsync($"{url}?client_id={clientId}&client_secret={clientSecret}");
-            var result = JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync());
+            var responsePayload = await response.Content.ReadAsStringAsync();
+            _logger.LogTrace(responsePayload);
+
+            var result = JsonConvert.DeserializeObject<T>(responsePayload);
 
             return result;
         }
