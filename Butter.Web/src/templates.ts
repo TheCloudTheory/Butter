8042b62e124cd897aba9a1d63ac9acec383e9ecc
Kamil Mrzygłód <kamil@thecloudtheory.com>
2019-10-03 09:00:28 +0200

    Test for Mustache on GitHub Pages

--- Butter.Web/src/templates.ts
+++ Butter.Web/src/templates.ts
@@ -3,7 +3,7 @@ import Mustache from 'mustache';
 
 export default class templates {
     static renderTemplate(name: string, wrapper: HTMLElement, model: Object): Promise<string> {
-        return http.getLocal(`templates/${name}.mst`).then(_ => {
+        return http.getLocal(`templates/${name}.js`).then(_ => {
             let rendered = Mustache.render(_, model);
             wrapper.innerHTML = rendered;
         }) as Promise<string>;
